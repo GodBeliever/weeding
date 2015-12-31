7a1e89e12a91a156f43ce0ab1a8bb8c382c9b2d0
GodBeliever <chief@lylyandalex.thankgod.fr>
2015-12-31 20:49:19 +0100

    Structure Of the project

---
+++ Wedding/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  	<title>Test PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Bonjour le monde</p>'; ?>
+ </body>
+</html>
